ec9006075e3a7af51e414a1e2396be64505e5569
Fox.Li <jiaqingli@gmail.com>
2020-06-23 22:31:00 +0800

    add comments

---
+++ src/common/README.md
@@ -0,0 +1,3 @@
+### Minx-in Pattern
+
+js 的一种设计模式, 通过直接给 prototype 添加其它类的方法来实现继承
--- src/common/mixinSample.ts
+++ src/common/mixinSample.ts
@@ -1,43 +1,47 @@
 class Disposable {
-    isDisposed: boolean;
-    dispose(): void {
-        this.isDisposed = true
-    }
+  isDisposed: boolean;
+  dispose(): void {
+    this.isDisposed = true;
+  }
 }
 
 class Activatable {
-    isActive: boolean;
-    activate():void {
-        this.isActive = true;
-    }
-    deactivate():void {
-        this.isActive = false;
-    }
+  isActive: boolean;
+  activate(): void {
+    this.isActive = true;
+  }
+  deactivate(): void {
+    this.isActive = false;
+  }
 }
 
 class SmartObject {
-    constructor(){
-        setInterval(() => console.log(this.isActive + " 1 : 2 " + this.isDisposed), 500);
-    }
-    interact(): void{
-        this.activate();
-    }
+  constructor() {
+    setInterval(() => console.log(this.isActive + ' 1 : 2 ' + this.isDisposed), 500);
+  }
+  interact(): void {
+    this.activate();
+  }
 }
 
 interface SmartObject extends Disposable, Activatable {}
 
 function applyMixins(derivedCtor: any, baseCtors: any[]): void {
   baseCtors.forEach((baseCtor) => {
-    Object.getOwnPropertyNames(baseCtor.prototype).forEach( name => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name)!);
     });
   });
 }
 
-applyMixins(SmartObject, [Disposable, Activatable])
+applyMixins(SmartObject, [Disposable, Activatable]);
 
-const smartObject = new SmartObject()
-setTimeout(()=> smartObject.interact(), 1000)
-setTimeout(() => smartObject.dispose(), 1500);
-
+const smartObject = new SmartObject();
+setTimeout(() => smartObject.interact(), 1000); //1 秒之后打印的 isActive 是 true, 之前是 undefined
+setTimeout(() => smartObject.dispose(), 1500); //1.5 秒之后打印的 isDisposed 是true, 之前是 undefined
+/**output
+undefined 1 : 2 undefined
+true 1 : 2 undefined
+true 1 : 2 true 
+ **/
